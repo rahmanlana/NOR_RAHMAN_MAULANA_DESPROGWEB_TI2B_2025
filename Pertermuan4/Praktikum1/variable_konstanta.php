702fee2f08e00f93d921bd5bc0fd37f69bdf8347
rahmanlana <rahmanlana044@gmail.com>
2025-09-22 11:03:39 +0700

    PRAKTIKUM1_DANPRAKTIKUM2

---
+++ Pertermuan4/Praktikum1/variable_konstanta.php
@@ -0,0 +1,16 @@
+<?php 
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "hasil Penjumlahan $angka1 dan $angka2 adalah $hasil.";
+
+$benar = true;
+$salah = false;
+echo "Variable benar: $benar, variable salah: $salah";
+
+define("nama_situs", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2023);
+
+echo "Selamat datang di ". nama_situs .", Situsyang didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
+
+?>
---
+++ Pertermuan4/Praktikum2/tipe_data.php
@@ -0,0 +1,47 @@
+<?php 
+$a = 10;
+$b = 5;
+$c = $a + 5;
+$d = $b + (10*5);
+$e = $d -$c;
+
+echo "Variable a: {$a} <br>"
+echo "Variable b: {$b} <br>"
+echo "Variable c: {$c} <br>"
+echo "Variable d: {$d} <br>"
+echo "Variable e: {$e} <br>"
+
+var_dump($e);
+
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) /3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Matematika: {$rataRata} <br>";
+
+var_dump($rataRata);
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahLulusUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahLulusUjian);
+
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
+
+echo "Nama Depan: {$namaDepan} <br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+
+echo $namaLengkap;
+
+?>
